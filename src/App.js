9af8cf2a397bcf93b2973b0fdc4c64262e020d65
ben.dagana@grimm-co.com <ben.dagana@grimm-co.com>
2021-06-24 15:02:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hasta La Vista Baby!
         </p>
         <a
           className="App-link"
